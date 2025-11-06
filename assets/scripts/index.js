a35726002a5c40a0828b47ddff1c116bd3325b38
kiwicat-minecraft <emgekilian@outlook.de>
2025-11-06 20:08:15 +0100

    nice shit

---
+++ assets/scripts/index.js
@@ -0,0 +1,12 @@
+//leave this here forever
+console.log("UwU");
+console.log("Why are you Here in My Home?");
+console.log("Stop spying and go to my youtube :3");
+
+//let is variable or var but he wanted to use let beacuse he is a fancy tutorial guy
+
+//document.getElementById("shit").textContent = "Shit";
+
+//var Kiwi = "Kiwi";
+//console.log(Kiwi)
+
