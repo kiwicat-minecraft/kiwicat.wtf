7cb8063e00dd202d52daf589fba28769f757d2da
kiwicat-minecraft <emgekilian@outlook.de>
2025-11-08 16:05:42 +0100

    guest prep

---
+++ assets/scripts/guest.js
@@ -0,0 +1,7 @@
+
+var text;
+
+document.getElementById("g_sub").onclick = function(){
+    text = document.getElementById("g_in").value;
+    console.log(text)
+}
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -10,3 +10,17 @@ console.log("Stop spying and go to my youtube :3");
 //var Kiwi = "Kiwi";
 //console.log(Kiwi)
 
+//Math.random() is random
+//.checked for checkboxes
+
+//nice if with
+//let message = age >= 18 ? "adult" : "minor" makes message adult if 18+ and if not minor
+
+//.charAt(x) on string gives x-1 letter
+//.indexOf(O) gives the first pos of the letter O
+//.length gives length
+//.trim() get rid of emptieness
+//.toUpperCase() makes it upcase
+//.includes() for stuff included
+//.replaceAll("-","/") makes - to /
+
