90039d68d08d4d3a284ef812a4ecc77308cef7fb
kiwicat-minecraft <emgekilian@outlook.de>
2025-11-15 11:46:01 +0100

    url adder for skins

--- assets/scripts/skins.js
+++ assets/scripts/skins.js
@@ -1,26 +1,39 @@
-fetchData()
 
-async function fetchData(){
+
+window.addEventListener("DOMContentLoaded", () => {
+    const params = new URLSearchParams(window.location.search);
+    const q = params.get("skin");
+
+    if (q) {
+        document.getElementById("skin_in").value = q;
+        fetchData(q);
+    }
+});
+
+
+
+async function fetchData(forceName){
 
     try{
-        const pokemonName = document.getElementById("skin_in").value;
+       
+        const pokemonName = forceName || document.getElementById("skin_in").value;
 
-        const response = await fetch(`https://minotar.net/armor/body/${pokemonName}/100.png`)
+        
+        const newURL = window.location.pathname + "?skin=" + encodeURIComponent(pokemonName);
+        history.pushState({}, "", newURL);
+
+        
+        const response = await fetch(`https://minotar.net/armor/body/${pokemonName}/100.png`);
 
         if(!response.ok){
-            throw new Error("couldnt find that Skin sowwy")
+            throw new Error("couldnt find that Skin sowwy");
         }
 
-        
-        
         const pokemonSprite = `https://minotar.net/armor/body/${pokemonName}/100.png`;
         const imgEle = document.getElementById("pokemon_sprite");
         const nameEle = document.getElementById("name");
-        nameEle.textContent = pokemonName;
-        
-        
-        
 
+        nameEle.textContent = pokemonName;
         imgEle.src = pokemonSprite;
         imgEle.style.display = "block";
         
