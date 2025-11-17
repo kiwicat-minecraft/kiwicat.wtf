f3ec905f3974af26cceed895b66e3767b6c86ec9
kiwicat-minecraft <emgekilian@outlook.de>
2025-11-17 20:42:33 +0100

    fix 1

--- assets/scripts/skins.js
+++ assets/scripts/skins.js
@@ -18,7 +18,7 @@ async function fetchData(forceName){
     try{
        
         const pokemonName = forceName || document.getElementById("skin_in").value;
-        document.getElementById("embed").src = `https://minotar.net/armor/body/${pokemonName}/100.png`
+        document.getElementById("embed").content = `https://minotar.net/armor/body/${pokemonName}/100.png`
 
         
         const newURL = window.location.pathname + "?skin=" + encodeURIComponent(pokemonName);
